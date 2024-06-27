3c46fdb0afa1d615a9ceda1f61a1d8b5af8441fd
Ardiyasaa <ardiyasadps18@gmail.com>
2024-06-27 19:27:38 +0800

    Add files via upload

---
+++ kerja.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 27, 2024 at 01:26 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kerja`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `id_absensi` int(11) NOT NULL,
+  `id_karyawan` int(11) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `jam_masuk` time DEFAULT NULL,
+  `jam_pulang` time DEFAULT NULL,
+  `status_absensi` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `absensi`
+--
+DELIMITER $$
+CREATE TRIGGER `trg_auto_generate_absensi_id` BEFORE INSERT ON `absensi` FOR EACH ROW BEGIN
+    SET NEW.id_absensi = UUID();
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gaji`
+--
+
+CREATE TABLE `gaji` (
+  `id_gaji` int(11) NOT NULL,
+  `id_karyawan` int(11) DEFAULT NULL,
+  `gaji_pokok` decimal(10,2) DEFAULT NULL,
+  `tunjangan` decimal(10,2) DEFAULT NULL,
+  `potongan` decimal(10,2) DEFAULT NULL,
+  `total_gaji` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `nik` varchar(20) DEFAULT NULL,
+  `jabatan` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `vw_karyawan_absensi`
+--
+CREATE TABLE `vw_karyawan_absensi` (
+`nama` varchar(50)
+,`tanggal` date
+,`jam_masuk` time
+,`jam_pulang` time
+,`status_absensi` varchar(20)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `vw_karyawan_absensi`
+--
+DROP TABLE IF EXISTS `vw_karyawan_absensi`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_karyawan_absensi`  AS  select `k`.`nama` AS `nama`,`a`.`tanggal` AS `tanggal`,`a`.`jam_masuk` AS `jam_masuk`,`a`.`jam_pulang` AS `jam_pulang`,`a`.`status_absensi` AS `status_absensi` from (`karyawan` `k` join `absensi` `a` on((`k`.`id_karyawan` = `a`.`id_karyawan`))) ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`id_absensi`);
+
+--
+-- Indexes for table `gaji`
+--
+ALTER TABLE `gaji`
+  ADD PRIMARY KEY (`id_gaji`);
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
